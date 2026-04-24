7d07085b801b95f4c383f2643bd3edfdfbca4d73
Jair León <leonrhj@gmail.com>
2026-04-24 16:29:50 -0600

    add favicon and configs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.100.238"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "cdn.discordapp.com" },
